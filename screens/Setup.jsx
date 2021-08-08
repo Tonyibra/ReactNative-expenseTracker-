b4d38d28e85bc77563d9209171155ec9541568d1
tonyibra <tonyibra.ads@gmail.com>
2021-08-08 03:25:15 +0300

    setupscreen done

--- screens/Setup.jsx
+++ screens/Setup.jsx
@@ -1,13 +1,48 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
+import { PrimaryBtn } from "../Components/Buttons/PrimaryBtn";
 
 export const Setup = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Let’s setup your account!</Text>
+      <View style={styles.Contentcontainer}>
+        <Text style={styles.setupText}>Let’s setup your account!</Text>
+        <Text style={styles.subTitle}>
+          Account can be your bank, credit card or your wallet.
+        </Text>
+      </View>
+      <View style={styles.btnWrapper}>
+        <PrimaryBtn title="Let’s go" />
+      </View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  setupText: {
+    marginTop: 70,
+    fontSize: 39,
+    lineHeight: 44,
+    fontWeight: "500",
+    color: "#212325",
+  },
+  subTitle: {
+    maxWidth: "70%",
+    marginTop: 40,
+    fontSize: 14,
+    lineHeight: 18,
+    fontWeight: "500",
+    color: "#292B2D",
+  },
+  container: {
+    flex: 1,
+    margin: 16,
+  },
+  Contentcontainer: {
+    flex: 1,
+  },
+  btnWrapper: {
+    alignItems: "center",
+  },
+});
